b25edc315d714df82788b5dac62d2484b5eca15e
thariq maulana <thariqmaulana01@gmail.com>
2024-02-26 05:32:54 +0700

    ..

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -1,8 +1,8 @@
 <template>
   <form>
     <div class="flex justify-center min-h-screen items-center">
-      <div class="w-full max-w-xs bg-indigo-600 rounded-lg">
-        <div class="w-full bg-teal-950">
+      <div class="w-full h-full max-h-full max-w-xs bg-indigo-600 rounded-lg">
+        <div class="w-full bg-purple-800">
           <h1 class="text-2xl text-white text-center mb-2 font-bold p-3">
             Login
           </h1>
@@ -12,7 +12,7 @@
             placeholder=" username"
             type="text"
             name="username"
-          fq  id="username"
+            id="username"
             class="border-solid border border-black mb-4 font-medium h-8 w-3/4"
             :modelValue="inputUsername"
             @update:modelValue="newUsername => inputUsername = newUsername"
@@ -21,7 +21,7 @@
         <div class="text-center">
           <Input
             placeholder=" password"
-            type="text"
+            type="password"
             name="password"
             id="password"
             class="border-solid border border-black font-medium h-8 w-3/4"
@@ -32,22 +32,32 @@
         <div class="text-center">
           <button
             @click.prevent="sendData"
+            :isSuccess="isSuccess"
             class="w-3/6 mt-4 font-bold bg-violet-500 p-2 rounded-sm text-white"
           >
             Login
           </button>
         </div>
+        <div class="text-center mt-4 text-white">
+          Dont have an <router-link to="/register" class="text-black hover:text-white">account?</router-link>
+        </div>
+        <div id="unathorized" class="text-center mt-4 text-white">
+          {{ message }}
+        </div>
       </div>
     </div>
   </form>
+
+  
 </template>
 
 <script>
 import axios from "axios";
 import Input from "../components/Input.vue";
+import router from "../router";
 
 export default {
-  props: [  ],
+  props: [],
   components: {
     Input,
   },
@@ -55,6 +65,8 @@ export default {
     return {
       inputUsername: '',
       inputPassword: '',
+      isSuccess: false,
+      message: ''
     };
   },
   methods: {
@@ -66,8 +78,13 @@ export default {
           password: this.inputPassword,
         });
         console.info(response);
+        this.isSuccess = true
+        router.replace('/home')
       } catch (error) {
         console.info(error.response.data.errors);
+        this.isSuccess = false
+        this.message = error.response.data.errors
+
       }
     },
   },
